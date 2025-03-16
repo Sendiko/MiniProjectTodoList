b8caaa7df415ec8051beeade7d6069f6ce57c209
Sendiko <sarangtawon897@gmail.com>
2025-03-16 11:33:49 +0700

    Implement Login Screen with basic functionality
    - Created `LoginState` to manage the state of the login screen.
    - Created `LoginEvent` for handling user interactions in the login screen.
    - Created `LoginViewModel` to manage login logic and state updates.
    - Implemented `LoginScreen` with input fields for email and password.
    - Added password visibility toggle functionality.
    - Added navigation to the register screen via a `TextButton`.
    - Added placeholder navigation to the dashboard screen via a `Button`.
    - Added dialog to show the result of the state
    - Added image in the login screen.

---
+++ app/src/main/java/org/cheva/miniprojecttodolist/login/LoginViewModel.kt
@@ -0,0 +1,56 @@
+package org.cheva.miniprojecttodolist.login
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class LoginViewModel: ViewModel() {
+
+    private val _state = MutableStateFlow(LoginState())
+    val state = _state.asStateFlow()
+
+    fun onEvent(event: LoginEvent) {
+        when(event){
+            is LoginEvent.OnEmailChanged -> changeEmail(event.email)
+            is LoginEvent.OnPasswordChanged -> changePassword(event.password)
+            is LoginEvent.OnPasswordVisibilityChanged -> changePasswordVisibility(event.isVisible)
+            LoginEvent.OnDismissDialog -> dismissDialog()
+            LoginEvent.OnLoginClicked -> login()
+        }
+    }
+
+    private fun changeEmail(email: String) {
+        _state.update {
+            it.copy(email = email)
+        }
+    }
+
+    private fun changePassword(password: String) {
+        _state.update {
+            it.copy(password = password)
+        }
+    }
+
+    private fun changePasswordVisibility(isVisible: Boolean) {
+        _state.update {
+            it.copy(
+                passwordVisible = isVisible
+            )
+        }
+    }
+
+    private fun dismissDialog() {
+        _state.update {
+            it.copy(
+                message = "",
+                successLogin = false
+            )
+        }
+    }
+
+    private fun login() {
+
+    }
+
+}
