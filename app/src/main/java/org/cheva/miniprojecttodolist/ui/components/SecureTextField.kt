8f7038102965703669e452a70099811676337696
Sendiko <sarangtawon897@gmail.com>
2025-03-16 10:02:13 +0700

    Refactor TextField and SecureTextField to use consistent styling
    - In `TextField.kt` and `SecureTextField.kt`, updated the `OutlinedTextField` to have consistent styling for focused and unfocused states.
    - Applied background and border colors for focused and unfocused states via `OutlinedTextFieldDefaults.colors`.
    - Set the `unfocusedContainerColor` and `unfocusedBorderColor` to `surfaceVariant`.
    - Set the `focusedContainerColor` and `focusedBorderColor` to `secondaryContainer`.

--- app/src/main/java/org/cheva/miniprojecttodolist/ui/components/SecureTextField.kt
+++ app/src/main/java/org/cheva/miniprojecttodolist/ui/components/SecureTextField.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -97,7 +98,13 @@ fun SecureTextField(
             visualTransformation = if (isVisible)
                     VisualTransformation.None
                 else
-                    PasswordVisualTransformation()
+                    PasswordVisualTransformation(),
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedBorderColor = MaterialTheme.colorScheme.surfaceVariant,
+                focusedBorderColor = MaterialTheme.colorScheme.secondaryContainer,
+            )
         )
     }
 }
--- app/src/main/java/org/cheva/miniprojecttodolist/ui/components/TextField.kt
+++ app/src/main/java/org/cheva/miniprojecttodolist/ui/components/TextField.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.ImeAction
@@ -94,7 +94,13 @@ fun OutlinedTextField(
                 keyboardType = keyboardType,
                 imeAction = imeAction,
             ),
-            keyboardActions = action
+            keyboardActions = action,
+            colors = OutlinedTextFieldDefaults.colors(
+                unfocusedContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedBorderColor = MaterialTheme.colorScheme.surfaceVariant,
+                focusedBorderColor = MaterialTheme.colorScheme.secondaryContainer,
+            )
         )
     }
 }
