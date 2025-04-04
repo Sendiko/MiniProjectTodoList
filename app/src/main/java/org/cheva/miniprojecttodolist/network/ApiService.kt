d92dcb1b650e0a659408911ef10499f6e41955cd
Sendiko <sarangtawon897@gmail.com>
2025-04-05 00:00:43 +0700

    creating retrofit and api service interface

--- app/src/main/java/org/cheva/miniprojecttodolist/network/ApiService.kt
+++ app/src/main/java/org/cheva/miniprojecttodolist/network/ApiService.kt
@@ -1,4 +1,16 @@
 package org.cheva.miniprojecttodolist.network
 
+import org.cheva.miniprojecttodolist.register.data.RegisterRequest
+import org.cheva.miniprojecttodolist.register.data.RegisterResponse
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
 interface ApiService {
+
+    @POST("register")
+    suspend fun register(
+        @Body request: RegisterRequest
+    ): Call<RegisterResponse>
+
 }
--- app/src/main/java/org/cheva/miniprojecttodolist/network/Retrofit.kt
+++ app/src/main/java/org/cheva/miniprojecttodolist/network/Retrofit.kt
@@ -1,4 +1,21 @@
 package org.cheva.miniprojecttodolist.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
 object Retrofit {
+
+    fun provideClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                chain.proceed(chain.request().newBuilder().also {
+                    it.addHeader("Accept", "application/json")
+                }.build())
+            }.also {
+                val logging = HttpLoggingInterceptor()
+                logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+                it.addInterceptor(logging)
+            }.build()
+    }
+
 }
