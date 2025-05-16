7f848726eacd1774e382bd345136adf0db49878f
Sendiko <sarangtawon897@gmail.com>
2025-05-16 09:19:39 +0700

    refactor(todo): update OnTodoClicked event parameter type

--- app/src/main/java/org/cheva/miniprojecttodolist/todo/list/presentation/DashboardEvent.kt
+++ app/src/main/java/org/cheva/miniprojecttodolist/todo/list/presentation/DashboardEvent.kt
@@ -1,9 +1,9 @@
 package org.cheva.miniprojecttodolist.todo.list.presentation
 
-import org.cheva.miniprojecttodolist.todo.core.data.Todo
+import org.cheva.miniprojecttodolist.todo.list.data.TodosItem
 
 sealed interface DashboardEvent {
     data class OnTodoChecked(val isChecked: Boolean): DashboardEvent
-    data class OnTodoClicked(val todo: Todo): DashboardEvent
+    data class OnTodoClicked(val todo: TodosItem): DashboardEvent
     data object OnLoadTodo: DashboardEvent
 }
